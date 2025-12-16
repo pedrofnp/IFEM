0694f741a62f4b60c578cae48886a61467a555af
Pedro <pedro.machado@fnp.org.br>
2025-12-16 16:22:04 -0300

    feat: nova landing do IFEM como página inicial

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,18 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('', include('map.urls')),
-    path('', include('detail.urls')),
+    path("admin/", admin.site.urls),
+
+    # 1. Landing do IFEM como home
+    path("", include("ifem.urls")),
+
+    # 3. Análise Gráfica
+    path("analise/", include("home.urls")),
+
+    # 4. Análise Municipal
+    path("mapa/", include("map.urls")),
+
+    # 5. Análise Agregada
+    path("analise-municipal/", include("detail.urls")),
 ]
