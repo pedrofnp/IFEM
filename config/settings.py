447368c71f171b591b957aa9149a9f7834a59b2a
Pedro <pedro.machado@fnp.org.br>
2026-03-16 19:00:12 -0300

    fix: restaura MAPBOX_PUBLIC_TOKEN ausente

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
+MAPBOX_PUBLIC_TOKEN = os.getenv("MAPBOX_PUBLIC_TOKEN", "")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
